7763cf74f873a457d59c27c6ab6f1edb9105387e
akshayzoenix <84169151+akshayzoenix@users.noreply.github.com>
2025-05-28 18:55:15 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("admin");       // ✅ this makes /admin and config.yml accessible
+  eleventyConfig.addPassthroughCopy("images");      // ✅ (optional) if you have an images folder
 
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md").reverse();
